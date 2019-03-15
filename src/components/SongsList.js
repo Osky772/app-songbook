dd17dc72ed45456b7cde27fbfd26cb17fa072a9e
Osky772 <kowalow.oskar@gmail.com>
2019-03-15 22:32:58 +0100

    Little styles on rrendered songs list

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -2,6 +2,14 @@ import React, { Component, Fragment } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
+const styles = {
+	paper: {
+		padding: "15px 20px",
+		margin: "10px 10px",
+		borderRadius: 0
+	}
+};
+
 class SongsList extends Component {
 	state = {
 		songs: []
@@ -23,7 +31,7 @@ class SongsList extends Component {
 		return (
 			<Fragment>
 				{songs.map(song => (
-					<Paper elevation={1}>
+					<Paper elevation={1} style={styles.paper}>
 						<Typography variant="h5" component="h3">
 							{song.performer + " - " + song.title}
 						</Typography>
