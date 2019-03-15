21f993dc32d4bba53ef244378e0d5611e168611b
Osky772 <kowalow.oskar@gmail.com>
2019-03-15 21:26:17 +0100

    Add static tabs

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 class Header extends Component {
 	render() {
@@ -11,6 +13,10 @@ class Header extends Component {
 					<Typography variant="h6" color="inherit">
 						Śpiewnik
 					</Typography>
+					<Tabs value={0}>
+						<Tab label="Lista piosenek" />
+						<Tab label="Playilisty" />
+					</Tabs>
 				</Toolbar>
 			</AppBar>
 		);
