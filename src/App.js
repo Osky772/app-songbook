4be99d3e930a9ddf5c715278aa879a1bb89527de
Osky772 <kowalow.oskar@gmail.com>
2019-03-15 21:01:31 +0100

    test fetching data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+const data = { title: "im here!!!" };
+
+async function fetchSongs() {
+	const songs = await fetch("data.json").then(res => res.json());
+	console.log(songs);
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	add() {
+		fetch("http://localhost:3000/data.json", {
+			method: "POST",
+			body: JSON.stringify(data)
+		})
+			.then(res => res.json())
+			.catch(err => console.log("Error", err))
+			.then(res => console.log("Success", res));
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={() => this.add()}>Add</button>
+				<button onClick={() => fetchSongs()}>Fetch</button>
+			</div>
+		);
+	}
 }
 
 export default App;
