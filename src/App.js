4ecd0b4d5e62030fd39aa4811c185762a80515f5
Osky772 <kowalow.oskar@gmail.com>
2019-03-17 14:33:37 +0100

    Style header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Playlists from "./components/Playlists";
 import SongsList from "./components/SongsList";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
 import { BrowserRouter, Route } from "react-router-dom";
 import Song from "./components/Song";
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,15 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Grid from "@material-ui/core/Grid";
 
 const styles = {
 	Toolbar: {
 		padding: "5px 10px 0 10px"
+	},
+	maxWidth: {
+		maxWidth: 1030,
+		margin: "0 auto"
 	}
 };
 
@@ -25,21 +30,31 @@ class Header extends Component {
 		const { value } = this.state;
 		return (
 			<AppBar position="static" color="primary">
-				<Toolbar variant="dense" style={styles.Toolbar}>
-					<Typography
-						component={Link}
-						to="/lista-piosenek"
-						variant="h6"
-						color="inherit"
-						style={{ textDecoration: "none" }}
-					>
-						Śpiewnik
-					</Typography>
-					<Tabs value={value} onChange={this.handleChange}>
-						<Tab component={Link} to="/lista-piosenek" label="Lista piosenek" />
-						<Tab component={Link} to="/playlisty" label="Playilisty" />
-					</Tabs>
-				</Toolbar>
+				<Grid container style={styles.maxWidth}>
+					<Toolbar variant="dense" style={styles.Toolbar}>
+						<Grid item lg={4}>
+							<Typography
+								component={Link}
+								to="/lista-piosenek"
+								variant="h6"
+								color="inherit"
+								style={{ textDecoration: "none" }}
+							>
+								Śpiewnik
+							</Typography>
+						</Grid>
+						<Grid item lg={8}>
+							<Tabs value={value} onChange={this.handleChange}>
+								<Tab
+									component={Link}
+									to="/lista-piosenek"
+									label="Lista piosenek"
+								/>
+								<Tab component={Link} to="/playlisty" label="Playilisty" />
+							</Tabs>
+						</Grid>
+					</Toolbar>
+				</Grid>
 			</AppBar>
 		);
 	}
