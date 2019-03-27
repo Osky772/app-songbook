eb85db5f11ea088c4f9a3f46c8aecc1d4d3e4ba3
Osky772 <kowalow.oskar@gmail.com>
2019-03-27 12:00:15 +0100

    Change input form from class to funciton compoennt

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import TextField from "@material-ui/core/TextField";
 
-class SearchForm extends Component {
-	state = {};
-	render() {
-		return (
-			<form noValidate autoComplete="off">
-				<TextField
-					id="standard-full-width"
-					label="Wyszukaj piosenkę"
-					style={{ margin: 8 }}
-					placeholder="Wpisz nazwę artysty lub tytuł piosenki..."
-					fullWidth
-					onChange={this.props.handleChange}
-					margin="normal"
-					InputLabelProps={{
-						shrink: true
-					}}
-				/>
-			</form>
-		);
-	}
-}
+const SearchForm = ({ handleChange }) => {
+	return (
+		<form noValidate autoComplete="off">
+			<TextField
+				id="standard-full-width"
+				label="Wyszukaj piosenkę"
+				style={{ margin: 8 }}
+				placeholder="Wpisz nazwę artysty lub tytuł piosenki..."
+				fullWidth
+				onChange={handleChange}
+				margin="normal"
+				InputLabelProps={{
+					shrink: true
+				}}
+			/>
+		</form>
+	);
+};
 
 export default SearchForm;
