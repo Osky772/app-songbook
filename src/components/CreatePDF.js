50491d55eecb8e7006e6bc1d273de9e12221a388
Osky772 <kowalow.oskar@gmail.com>
2019-03-29 22:42:25 +0100

    Save selected songs in state in create pdf

--- src/components/CreatePDF.js
+++ src/components/CreatePDF.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 
 class CreatePDF extends Component {
-	state = {};
+	state = {
+		songs: []
+	};
+
+	static getDerivedStateFromProps(props, state) {
+		if (props.selectedSongs.length !== state.songs.length) {
+			return {
+				songs: props.selectedSongs
+			};
+		}
+		return null;
+	}
+
 	render() {
-		const { selectedSongs } = this.props;
 		return (
 			<Button
 				variant="outlined"
 				style={{ height: "40px" }}
-				onClick={() => console.log(selectedSongs)}
+				onClick={() => console.log(this.state.songs)}
 			>
 				Create PDF
 			</Button>
